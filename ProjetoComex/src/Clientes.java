c79b7d20da0b11e9f514bf480321511f350c2177
RaphaelFerrari <156969959+RaphaelFerrari@users.noreply.github.com>
2024-02-08 17:50:43 -0300

    Add files via upload

---
+++ ProjetoComex/src/Clientes.java
@@ -0,0 +1,15 @@
+public class Clientes {
+    public static void main(String[] args) {
+        String nome = "Fulano";
+        int dataDeNascimento = 1980;
+        System.out.println(">>>>> Listagem dos Clientes ");
+        System.out.println("-----------------------------");
+        System.out.println("\nNome: " + nome);
+        System.out.println("\nData de Nascimento: " + dataDeNascimento);
+
+        nome = "Cicrano";
+        dataDeNascimento = 2000;
+        System.out.println("\nNome: " + nome);
+        System.out.println("\nData de Nascimento: " + dataDeNascimento);
+      }
+}
---
+++ ProjetoComex/src/Produtos.java
@@ -0,0 +1,14 @@
+public class Produtos {
+    public static void main(String[] args) {
+        String nome = "Livro";
+        String preco = "R$ 49.95";
+        System.out.println(">>>>> Listagem dos Produtos ");
+        System.out.println("-----------------------------");
+        System.out.println("\nNome: " + nome);
+        System.out.println("\nPreço: " + preco);
+        nome = "Celular";
+        preco = "R$ 1470.76";
+        System.out.println("\nNome: " + nome);
+        System.out.println("\nPreço: " + preco); 
+    }
+}
